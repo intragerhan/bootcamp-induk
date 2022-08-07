81b89c919c0c76d405b0c9a87be0158551f56752
Bruce Han <intrager@naver.com>
2022-08-07 23:54:45 +0900

    Migrating Spring MVC to Spring Boot

---
+++ bootIndukBootcamp/src/main/java/com/bootcamp/induk/IndukApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.induk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndukApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IndukApplication.class, args);
+    }
+
+}
---
+++ bootIndukBootcamp/src/test/java/com/bootcamp/induk/IndukApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.induk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndukApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
